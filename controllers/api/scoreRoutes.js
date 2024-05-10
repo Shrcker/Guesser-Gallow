6cb6040aea84139137d594da9bc27be6885cb199
Tania <tanner.shirkey@gmail.com>
2024-05-09 21:14:41 -0400

    Added sql routes
    Added routes to database information and some basic routing for the homepage and login page

---
+++ controllers/api/scoreRoutes.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const { Score } = require("../../models");
+const withAuthorization = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const scoreData = await Score.findAll({
+      include: [{ model: User }],
+    });
+    res.status(200).json(scoreData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const scoreData = await Score.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+
+    if (!scoreData) {
+      res.status(404).json({ message: "No score was found with that id" });
+      return;
+    }
+
+    res.status(200).json(scoreData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/", withAuthorization, async (req, res) => {
+  try {
+    const scoreData = await Score.create({
+      user: req.body.user,
+      score: req.body.score,
+    });
+
+    res.status(200).json(scoreData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/", withAuthorization, async (req, res) => {
+  try {
+    const scoreData = await Score.findByPk(req.body.id, {
+      include: [{ model: User }],
+    });
+
+    if (!scoreData) {
+      res.status(404).json({ message: "No scores were found with that id" });
+      return;
+    }
+    
+    const updatedScore = await scoreData.update({
+      user_id: req.body.user_id,
+    });
+    res.status(200).json(updatedScore);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.delete("/:id", withAuthorization, async (req, res) => {
+  try {
+    const scoreData = await Score.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+
+    if (!scoreData) {
+      res.status(404).json({ message: "No scores wered found with that id" });
+    }
+
+    scoreData.destroy();
+    res.status(200).json("User deleted successfully!");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = router;
