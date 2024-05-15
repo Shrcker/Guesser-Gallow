441da4bd6bf1aa29acaf445c5f2ab713579833f8
Tania <tanner.shirkey@gmail.com>
2024-05-14 20:34:22 -0400

    fixed typo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(expres.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
