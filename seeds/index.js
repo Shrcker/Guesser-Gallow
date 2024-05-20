7d5a0eae039c344d26935869cde1c6a15e87e4dc
Tania <tanner.shirkey@gmail.com>
2024-05-20 17:59:57 -0400

    Added seeds

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+const sequelize = require("../config/connection");
+const { User, Score } = require("../models");
+
+const userData = require("./userData.json");
+const scoreData = require("./scoreData.json");
+
+const seedDatabase = async () => {
+	await sequelize.sync({ force: true });
+
+	const users = await User.bulkCreate(userData, {
+		individualHooks: true,
+		returning: true,
+	});
+
+	for (const score of scoreData) {
+		await Score.create({
+			...score,
+		});
+	}
+
+	process.exit(0);
+};
+
+seedDatabase();
