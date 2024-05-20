7ca083f222e7f6428474c51db31d4cdd87a52d94
Tania <tanner.shirkey@gmail.com>
2024-05-20 16:08:45 -0400

    Fixed login and logout buttons not working
    Assumed the issue with the last commit was a problem with frontend JS, but here I saw several typos in the routes that prevented them from functioning properly. There were several issues with the frontend too, but this should be working globally now

--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,51 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();  
-    // Collect values from the login form
-    const email = document.getElementById('username-login').value.trim();
-    const password = document.getElementById('password-login').value.trim();
-  
-    if (password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ name, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+	event.preventDefault();
+	// Collect values from the login form
+	const name = document.getElementById("username-login").value.trim();
+	const password = document.getElementById("password-login").value.trim();
 
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.getElementById('name-signup').value.trim();
-    const password = document.getElementById('password-signup').value.trim();
-  
-    if (name && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+	if (name && password) {
+		// Send a POST request to the API endpoint
+		const response = await fetch("/api/users/login", {
+			method: "POST",
+			body: JSON.stringify({ name, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			// If successful, redirect the browser to the profile page
+			document.location.replace("/profile");
+		} else {
+			alert(response.statusText);
+		}
+	}
 };
-  
-  document
-    .getElementById('login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .getElementById('signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	const name = document.getElementById("name-signup").value.trim();
+	const password = document.getElementById("password-signup").value.trim();
+
+	if (name && password) {
+		const response = await fetch("/api/users", {
+			method: "POST",
+			body: JSON.stringify({ name, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			document.location.replace("/");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+document
+	.getElementById("login-form")
+	.addEventListener("submit", loginFormHandler);
+
+document
+	.getElementById("signup-form")
+	.addEventListener("submit", signupFormHandler);
